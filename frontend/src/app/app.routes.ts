b069b6b491b35a98ebede1935dfb46d5f2b58c44
devini0101 <vinieduardodias18@gmail.com>
2026-08-30 12:23:45 -0300

    Added wrapper into the pages, where allows only already logged in users to access, and wrapps the main content with the sidebar and dinamically changes the main content

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,22 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
-import { Dashboard } from './components/dashboard/dashboard';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { MainLayout } from './pages/main-layout/main-layout';
+import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
-  { path: '', component: Login },
-  { path: 'dashboard', component: Dashboard }
+  { path: 'login', component: Login },
+
+  {
+    path: '',
+    component: MainLayout,
+    canActivate: [authGuard], // Protege todas as rotas filhas
+    children: [
+      { path: 'dashboard', component: Dashboard },
+      // { path: 'dashboard', component: Dashboard }, // outras páginas protegidas devem ter o msm padrão e estar aq
+    ]
+  },
+
+  //redirecionamento padrão
+  { path: '**', redirectTo: 'login', pathMatch: 'full' }
 ];

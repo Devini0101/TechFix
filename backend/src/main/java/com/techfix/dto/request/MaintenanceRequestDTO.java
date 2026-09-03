bdbb78c463929de4da6d0ba4ee93e619e2026700
devini0101 <vinieduardodias18@gmail.com>
2026-09-03 18:57:22 -0300

    add maintenance request dto, modal and fetch categories into the modal

--- backend/src/main/java/com/techfix/dto/request/MaintenanceRequestDTO.java
+++ backend/src/main/java/com/techfix/dto/request/MaintenanceRequestDTO.java
@@ -1,4 +1,11 @@
 package com.techfix.dto.request;
 
-public record MaintenanceRequestDTO() {
+import jakarta.validation.constraints.NotEmpty;
+
+public record MaintenanceRequestDTO(
+        @NotEmpty(message = "É obrigatório informar o item") String item,
+        String itemDescription,
+        @NotEmpty(message = "É obrigatório informar o defeito do item") String itemDefect,
+        @NotEmpty(message = "É obrigatório uma categoria para o item") String categoryCode
+) {
 }
--- frontend/src/app/components/modal/service-request-modal.ts
+++ frontend/src/app/components/modal/service-request-modal.ts
@@ -1,4 +1,5 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit, inject, signal } from '@angular/core';
+import { Category, CategoryService } from '../../core/services/CategoryService';
 
 @Component({
   selector: 'app-service-request-modal',
@@ -7,25 +8,47 @@ import { Component, Output, EventEmitter } from '@angular/core';
     <div class="fixed inset-0 z-50 flex items-center justify-center bg-black/10 backdrop-blur-xs">
 		<div class="relative w-full max-w-md p-6 bg-gray-700 rounded-xl shadow-2xl border border-gray-300">
 		<h3 class="text-xl font-semibold">Criar nova solicitação de serviço:</h3>
-		<form class="flex flex-col mt-2">
+		<form class="flex flex-col mt-2 gap-1.5">
 			<div class="form-group flex flex-col gap-2">
-				<p>Digite o Item do serviço</p>
-				<input type="text" placeholder="Digite aqui o tem no serviço" class="rounded p-2 border border-gray-200 focus:border-blue-900">
+				<div class="flex gap-1">Item do serviço <p class="text-red-700">*</p></div>
+				<input type="text" placeholder="Digite aqui o item no serviço" class="rounded p-2 border border-gray-200 focus:border-blue-900">
 			</div>
 			<div class="form-group flex flex-col gap-2">
-				<p>Digite o Item do serviço</p>
-				<input type="text" placeholder="Digite aqui o tem no serviço" class="rounded p-2 border border-gray-200 focus:border-blue-900">
+				<p>Descrição do item (opcional)</p>
+				<input type="text" placeholder="Descreva o item" class="rounded p-2 border border-gray-200 focus:border-blue-900">
+			</div>
+            <div class="form-group flex flex-col gap-2">
+				<div class="flex gap-1">Defeito do Item <p class="text-red-700">*</p></div>
+				<input type="text" placeholder="Digite aqui o defeito do item" class="rounded p-2 border border-gray-200 focus:border-blue-900">
+			</div>
+            <div class="form-group flex flex-col gap-2">
+				<div class="flex gap-1">Categoria <p class="text-red-700">*</p></div>
+                <select class="rounded p-2 border border-gray-200 focus:border-blue-900">
+                    <option selected hidden value="">-- Selecione uma categoria --</option>
+                    @for (cat of categories(); track cat.code) {
+                        <option class="bg-gray-700" [value]="cat.code">{{ cat.name }}</option>
+                    }
+                </select>
 			</div>
 		</form>
 		<div class="flex justify-end gap-3 mt-2">
-			<button (click)="closeModal.emit()" class="px-4 py-2 bg-gray-100 rounded-lg text-black border border-gray-200 hover:scale-105  hover:cursor-pointer">Cancel</button>
-			<button (click)="closeModal.emit()" class="px-4 py-2 text-white border border-white bg-green-800 rounded-lg hover:scale-105 hover:cursor-pointer">Confirm</button>
+			<button (click)="closeModal.emit()" class="px-4 py-2 bg-gray-100 rounded-lg text-black border border-gray-200 hover:scale-105  hover:cursor-pointer">Cancelar</button>
+			<button (click)="closeModal.emit()" class="px-4 py-2 text-white border border-white bg-green-800 rounded-lg hover:scale-105 hover:cursor-pointer">Confirmar</button>
 		</div>
 		</div>
 	</div>
   `
 })
-export class ServiceRequestModal {
-  // @Output permite que o modal avise quem o chamou que é hora de fechar
+export class ServiceRequestModal implements OnInit {
   @Output() closeModal = new EventEmitter<void>();
+  private categoryService = inject(CategoryService);
+  categories = signal<Category[]>([]);
+
+  ngOnInit(): void {
+    this.categoryService.getAll().subscribe({
+      next: (data) => {
+        this.categories.set(data);
+      }
+    });
+  }
 }
